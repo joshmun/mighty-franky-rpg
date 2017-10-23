1215ef5ea2d9b676d15ae2e417ca17b28b2772ef
Joshua Mun <joshuamun@gmail.com>
2017-10-23 11:28:43 -0700

    add constructor classes for rest of classes, instantiating player with parameters, refactored

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -13,5 +13,14 @@ namespace Engine
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+
+        public Monster(int id, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitpoints) : base(currentHitPoints, maximumHitpoints)
+        {
+            ID = id;
+            Name = name;
+            MaximumDamage = maximumDamage;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+        }
     }
 }
